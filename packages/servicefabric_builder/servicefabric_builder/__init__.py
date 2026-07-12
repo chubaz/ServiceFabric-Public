3d425320b9636ff27c81ad652470f95d9385bc5b
lorenzoccasoni <lorenzo.c.casoni@gmail.com>
2026-07-12 17:13:43 +0200

    feat: add reproducible artifact identity

--- packages/servicefabric_builder/servicefabric_builder/__init__.py
+++ packages/servicefabric_builder/servicefabric_builder/__init__.py
@@ -3,5 +3,6 @@
 from .portfolio import ApplicationPortfolio
 from .source import SourceValidationError, ValidatedSourceBundle, validate_source
 from .builder import BuildError, BuildOutput, BuildPolicy, StaticWebBuilder
+from .identity import artifact_manifest, build_input_digest, build_spec_digest
 
-__all__ = ["ApplicationPortfolio", "BuildError", "BuildOutput", "BuildPolicy", "StaticWebBuilder", "SourceValidationError", "ValidatedSourceBundle", "validate_source"]
+__all__ = ["ApplicationPortfolio", "BuildError", "BuildOutput", "BuildPolicy", "StaticWebBuilder", "SourceValidationError", "ValidatedSourceBundle", "artifact_manifest", "build_input_digest", "build_spec_digest", "validate_source"]
