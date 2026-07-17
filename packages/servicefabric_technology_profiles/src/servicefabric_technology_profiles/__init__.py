4075ff13a5655b447461e9934e616912b92a1576
lorenzoccasoni <lorenzo.c.casoni@gmail.com>
2026-07-17 23:10:09 +0200

    feat(technology-profiles): select reviewed technology profiles

---
+++ packages/servicefabric_technology_profiles/src/servicefabric_technology_profiles/__init__.py
@@ -0,0 +1,13 @@
+"""Reviewed selection of technology profiles without provider execution."""
+
+from .selection import (
+    TechnologyProfileRequest,
+    TechnologyProfileSelector,
+    TechnologyProfileSelectionError,
+)
+
+__all__ = [
+    "TechnologyProfileRequest",
+    "TechnologyProfileSelector",
+    "TechnologyProfileSelectionError",
+]
